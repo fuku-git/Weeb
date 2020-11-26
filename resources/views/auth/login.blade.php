d613cdfc43b081ccf16da76aea1dd67fded30177
Taiyoshi <popo0129tt35@gmail.com>
2020-11-26 12:52:38 +0000

    ログイン画面修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,6 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('ログイン') }}</div>
 
+
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
