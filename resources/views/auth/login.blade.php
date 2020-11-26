329bc602f1365b2ae294f043d5c91601a25b3449
Taiyoshi <popo0129tt35@gmail.com>
2020-11-26 13:21:13 +0000

    ログイン画面デザイン修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,6 +12,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+                        
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
 
